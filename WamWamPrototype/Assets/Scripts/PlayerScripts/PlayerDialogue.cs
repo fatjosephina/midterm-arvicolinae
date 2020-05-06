99bd71b412f9c0d91e3f92bd363545d70263f971
fatjosephina <55261891+fatjosephina@users.noreply.github.com>
2020-05-06 03:13:38 -0400

    Added true dialogue

--- WamWamPrototype/Assets/Scripts/PlayerScripts/PlayerDialogue.cs
+++ WamWamPrototype/Assets/Scripts/PlayerScripts/PlayerDialogue.cs
@@ -5,11 +5,51 @@ using UnityEngine;
 public class PlayerDialogue : MonoBehaviour
 {
     public GameObject playerHint;
-    public CameraMovement cameraMovement;
+    public GameObject spacePrompt;
+    private CameraMovement cameraMovement;
+    public Material[] dialogueTexts;
+    private GameObject textBoxText;
+    private Renderer textRenderer;
+    private int textIndex;
+
+    private void Start()
+    {
+        textBoxText = GameObject.Find("TextBoxText");
+        textRenderer = textBoxText.GetComponent<MeshRenderer>();
+        textIndex = 0;
+    }
 
     private void Update()
     {
         cameraMovement = GameObject.Find("Main Camera").GetComponent<CameraMovement>();
+        if (cameraMovement.moveState == CameraMovement.MoveState.Reading)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                if (textIndex < dialogueTexts.Length - 1)
+                {
+                    textIndex++;
+                }
+                textRenderer.material = dialogueTexts[textIndex];
+                if (textIndex == dialogueTexts.Length - 1 && spacePrompt.activeInHierarchy)
+                {
+                    spacePrompt.SetActive(false);
+                }
+            }
+        }
+        else
+        {
+            if (textIndex != 0)
+            {
+                textIndex = 0;
+                textRenderer.material = dialogueTexts[textIndex];
+            }
+            if (!spacePrompt.activeInHierarchy)
+            {
+                spacePrompt.SetActive(true);
+            }
+        }
+        Debug.Log(textIndex);
     }
 
     private void OnTriggerEnter(Collider other)
---
+++ WamWamPrototype/Assets/Scripts/SpacePrompt.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpacePrompt : MonoBehaviour
+{
+    private CameraMovement cameraMovement;
+    private float moveSpeed = 0.05f;
+    private int frames = 0;
+    private Vector3 startPosition = Vector3.zero;
+
+    private void Start()
+    {
+        cameraMovement = GameObject.Find("Main Camera").GetComponent<CameraMovement>();
+
+    }
+
+    private void Update()
+    {
+        if (GetComponentInParent<TextBox>().savePosition)
+        {
+            startPosition = transform.localPosition;
+        }
+        if (cameraMovement.moveState == CameraMovement.MoveState.Reading)
+        {
+            frames++;
+
+            if (frames < 25)
+            {
+                transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed);
+            }
+            else if (frames > 25)
+            {
+                transform.Translate(Vector3.back * Time.deltaTime * moveSpeed);
+            }
+
+            if (frames >= 50)
+            {
+                frames = 0;
+            }
+        }
+        else
+        {
+            if ((startPosition != Vector3.zero) && (transform.position != startPosition))
+            {
+                transform.localPosition = startPosition;
+            }
+        }
+    }
+}
